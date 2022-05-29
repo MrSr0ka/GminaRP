e9fb8b41bffffd466647b90ecc9f68075fc804e8
Michael Sroka <104931364+MrSr0ka@users.noreply.github.com>
2022-05-29 17:10:53 +0100

    Add files via upload

---
+++ gmina.js
@@ -0,0 +1,17 @@
+var clip = [
+  "Archiwum Gminne",
+  "Witaj w Gminie!",
+  "@matebo",
+  "Kuraaaa",
+  "Zgadij kto afczy",
+  "@groobeach",
+  "Andrzej Lubon",
+  "@edmund_krzak",
+  "@wooyekzbyshek",
+];
+
+window.addEventListener("load", go);
+
+window.addEventListener("load") = function () {
+  document.getElementById("landingClip").innerHTML = clip;
+};
